e4eb38796b3fbcb8bbb52524f31a22f03b5d05ba
Andre Sionek <andresionek91@gmail.com>
2020-10-26 01:28:36 +0000

    create github actions workflows

--- app.py
+++ app.py
@@ -3,15 +3,12 @@ from enum import Enum
 from aws_cdk import core
 from bootcamp_data_platform.data_lake import DataLake
 from bootcamp_data_platform.ingestion import AtomicEventsRawIngestion
+from common import Environment
+import os
 
-
-class Environment(Enum):
-    PRODUCTION = 'production'
-    STAGING = 'staging'
-    DEV = 'dev'
-
+environment = Environment[os.environ['ENVIRONMENT']]
 
 app = core.App()
-data_lake = DataLake(app, environment=Environment.PRODUCTION)
-AtomicEventsRawIngestion(app, environment=Environment.PRODUCTION, data_lake=data_lake)
+data_lake = DataLake(app, environment=environment)
+AtomicEventsRawIngestion(app, environment=environment, data_lake=data_lake)
 app.synth()
