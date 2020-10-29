a50ec27f8c5d88fddaf42499c095fab4b636e93f
Andre Sionek <andresionek91@gmail.com>
2020-10-29 18:49:57 +0000

    creates catalog stack

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from aws_cdk import core
 from bootcamp_data_platform.data_lake import DataLake
 from bootcamp_data_platform.ingestion import RawIngestion
 from common import Common, Environment
-from catalog import Catalog
+from catalog import GlueCatalog
 import os
 
 environment = Environment[os.environ['ENVIRONMENT']]
